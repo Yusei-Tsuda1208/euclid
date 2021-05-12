deb21a243bce426f237b0d10c49d1715042355cb
Yusei-Tsuda1208 <83846113+Yusei-Tsuda1208@users.noreply.github.com>
2021-05-12 20:29:37 +0900

    Add files via upload

--- euclid.js
+++ euclid.js
@@ -1,8 +1,8 @@
 document.getElementById("euclid").addEventListener("click",function (){
-    let a = document.getElementById("warareru");
-    let b = document.getElementById("waru");
+    let a = document.getElementById("warareru").value;
+    let b = document.getElementById("waru").value;
     let r;
-    
+    document.getElementById("ans").value += (a + b) + "\n";
     (a,b)=>{
         while(a=0){
             a%b = a;
@@ -15,8 +15,5 @@ document.getElementById("euclid").addEventListener("click",function (){
         }
         
     }
-    document.write("aiueo");
-    document.close();
-    document.write('A');
-    document.getElementById("ans").value += (x + "=" + y + "*" + q + "+" + r) + "\n";
+    document.getElementById("ans").value += (a + b) + "\n";
 });
